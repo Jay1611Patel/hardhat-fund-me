5af388b40b6a8dd195b16a7ce3418351af727f26
Jay1611Patel <jprp1611@gmail.com>
2025-08-22 11:13:47 -0400

    testing

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -4,7 +4,7 @@ const { developmentChains } = require("../../helper-hardhat-config")
 
 // testing the unit tests for the FundMe contracts
 // testing the unit tests --- IGNORE ---
-// This is a test for the FundMe contracts
+// This is a test for the FundMe 
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("FundMe", function () {
